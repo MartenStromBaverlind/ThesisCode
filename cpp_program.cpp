5a75d9cd52372444cab9d70233f09bfbfac3b57a
MartenStromBaverlind <msd20009@student.mdh.se>
2024-04-15 17:06:37 +0200

    moved problem files to single json, now chatgpt takes multiple test cases into account

--- cpp_program.cpp
+++ cpp_program.cpp
@@ -1,45 +1,80 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
-int main() {
 struct ListNode {
-      int val;
-      ListNode *next;
-      ListNode() : val(0), next(nullptr) {}
-      ListNode(int x) : val(x), next(nullptr) {}
-     ListNode(int x, ListNode *next) : val(x), next(next) {}
-  };
-
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 class Solution {
 public:
-ListNode* rotateRight(ListNode* head, int k) {
-     
-    if(head==NULL) return NULL;
-    if(head->next==NULL) return head;
-    vector<ListNode*> v;
-    ListNode* temp=head;
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head == NULL) return NULL;
+        if(head->next == NULL) return head;
 
-    while(temp){
-        v.push_back(temp);
-        temp=temp->next;
+        vector<ListNode*> v;
+        ListNode* temp = head;
+
+        while(temp){
+            v.push_back(temp);
+            temp = temp->next;
+        }
+        
+        int n = v.size();
+        k = k % n;
+
+        if(k == 0) return head;
+        
+        v[n-1]->next = head;
+        v[n-k-1]->next = NULL;
+        
+        return v[n-k];
     }
-    int n=v.size()-1;
-    int indx=abs(n-k)+1;
-    
-    if(indx==0) return head;
-    else{
-        v[indx-1]->next=NULL;
-        v[n]->next=head;
-        head=v[indx];
-        return head;
+};
+
+// Add a main function that calls the solution class
+int main() {
+    Solution sol;
+    vector<int> input1 = {1, 2, 3, 4, 5};
+    vector<int> input2 = {0, 1, 2};
+
+    ListNode* head1 = new ListNode(input1[0]);
+    ListNode* curr = head1;
+    for(int i = 1; i < input1.size(); i++) {
+        curr->next = new ListNode(input1[i]);
+        curr = curr->next;
     }
-}};
- 
 
+    ListNode* result1 = sol.rotateRight(head1, input2[0]);
 
-    
+    // Check the result
+    while(result1) {
+        cout << result1->val << " ";
+        result1 = result1->next;
+    }
+    cout << endl;
+
+    ListNode* head2 = new ListNode(input2[0]);
+    ListNode* curr2 = head2;
+    for(int i = 1; i < input2.size(); i++) {
+        curr2->next = new ListNode(input2[i]);
+        curr2 = curr2->next;
+    }
+
+    ListNode* result2 = sol.rotateRight(head2, input2[1]);
+
+    // Check the result
+    while(result2) {
+        cout << result2->val << " ";
+        result2 = result2->next;
+    }
+    cout << endl;
+
     return 0;
 }
+
+
     
 
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import subprocess 
 import os
-import shutil
-import filecmp
+import json
 from openai import OpenAI
 os.environ["OPENAI_API_KEY"] = open("API_KEY","r").read()
 
@@ -23,18 +22,22 @@ def compile_and_run_cpp_program():
         print("Compilation failed.") 
         print(compile_process.stderr.decode()) 
 
-def read_Leetcode_Problem(): 
-    with open("problemcodes.txt") as file_1, open("probleminputs.txt") as file_2, open("problemoutputs.txt") as file_3:
-        problem = file_1.read()
-        input   = file_2.read()
-        output  = file_3.read()
-    return problem,input,output
+def read_Leetcode_Problem(): #TODO store problems in one JSON 
+    with open("problemcodes.txt") as file_1, open("probleminputs.txt") as file_2, open("problemoutputs.txt") as file_3, open("problemparameters.txt") as file_4, open("problemstructs.txt") as file_5:
+        problem   = file_1.read()
+        input     = file_2.read()
+        output    = file_3.read()
+        parameter = file_4.read()
+        struct    = file_5.read()
+    return problem,input,output,parameter,struct
 
-def generate_Chatgpt_Prompt(problem,input,output): 
-  background       = "I'm getting runtime error in my c++ program, could you identify and fix the problem for me? The response should include "
-  requirement      = "The solution should be able to call the function within the solution class with the following inputs : " + input + " and the corresponding outputs should be: " + output + "Also, I dont wan't any comments"
-  current_solution = "This is my current solution that does not work " + problem + " The response should contain everything needed so that the solution can be compiled and run instantly" 
-  return background + requirement + current_solution  
+def generate_Chatgpt_Prompt(code,inputs,outputs,params):
+  background       = "I'm getting runtime error in my c++ program, could you identify and fix the problem for me? Your solution should remove the runtime error and be able to pass the test case as described:"
+  requirements     = "In your response I want the following: Add a main function that calls the solution class with the following parameter(s)" , params , " where the inputs used are " , inputs , " and the output should be " , outputs , "Also I don't want any comments."
+  current_solution = "This is my current solution that does not work " + code + " The response should contain everything needed so that the solution can be compiled and run instantly, only modify the solution class do not remove any code that exists before it" 
+  includes         = "Do not include any libraries in your response, assume that they are already there"
+  # show previous prompts that did not work 
+  return str(background) + str(requirements) +  str(current_solution) +  str(includes)
 
 def send_Chatgpt_Request(prompt):  
   client = OpenAI()
@@ -45,42 +48,60 @@ def send_Chatgpt_Request(prompt):
   ]
 )
   return response.choices[0].message.content
+
+def reset_cpp_program(filename):
+    with open("cpp_includes.txt", 'r') as source_file:
+        content = source_file.read()
     
-def write_problem_to_cpp_program(filename,string_to_write):
-    if not (filecmp.cmp('cpp_skeleton.cpp', 'cpp_program.cpp')): # Check if we need to reset the cpp_program (has run atleast once)
-        shutil.copyfile('cpp_skeleton.cpp', 'cpp_program.cpp') 
-        
-    with open(filename, "r") as file:
+    with open(filename, 'w') as file:
+        file.write(content)
+
+    print(f"File {filename} has been overwritten.")
+
+def append_to_first_empty_line(filename, content_to_append):
+    with open(filename, 'r') as file:
         lines = file.readlines()
 
-    start_index = -1
-    end_index   = -1
-
-    # Find the start and end index of the main function
+    # Find the first empty line
+    line_number = None
     for i, line in enumerate(lines):
-        if line.strip() == "int main() {":
-            start_index = i + 1  # Start writing after the function declaration
-        elif start_index != -1 and line.strip() == "}":
-            end_index = i
+        if line.strip() == '':
+            line_number = i
             break
 
-    if start_index == -1 or end_index == -1:
-        print("Main function not found in", filename)
-        return
+    if line_number is not None:
+        # Insert the content after the first empty line
+        lines.insert(line_number, content_to_append + '\n')
 
-    # Write the string to the file within the main function
-    lines.insert(start_index, f'{string_to_write} \n')
+        # Write the modified content back to the file
+        with open(filename, 'w') as file:
+            file.writelines(lines)
+    else:
+        print("No empty line found in the file.")
 
-    # Write the modified content back to the file
-    with open(filename, "w") as file:
-        file.writelines(lines)
+def read_problems_json():
+    with open('leetcode_problems.json', 'r') as file:
+        json_data = json.load(file)
 
+    params  = json_data["problems"][0]["params"]
+    inputs  = json_data["problems"][0]["inputs"]
+    outputs = json_data["problems"][0]["outputs"]
+    struct  = json_data["problems"][0]["struct"]
+    code    = json_data["problems"][0]["code"]
+    print("Inputs: " , inputs)  
+    print("Outputs: " , outputs)  
+    print("Params: " , params)    
+    return code,inputs,outputs,params,struct
+    
 def main():
- problem,input,output = read_Leetcode_Problem()                           # Store the problem and test case input/output
- prompt               = generate_Chatgpt_Prompt(problem,input,output)     # Generate the prompt for chatgpt 
- response             = send_Chatgpt_Request(prompt)                      # Send request to chatgpt and store response
- write_problem_to_cpp_program("cpp_program.cpp",response)                 # Insert the the response into cpp skeleton
- compile_and_run_cpp_program()                                            # Compile and run cpp program
+    code,inputs,outputs,params,struct = read_problems_json()
+ #problem,input,output,parameter,struct = read_Leetcode_Problem()                              
+    prompt                         = generate_Chatgpt_Prompt(code,inputs,outputs,params)         # Generate the prompt for chatgpt 
+    response                       = send_Chatgpt_Request(prompt)                                # Send request to chatgpt and store response
+    reset_cpp_program("cpp_program.cpp")                                                         # Overwrite the cpp file with only libraries to avoid chatgpt from adding its own libraries
+    append_to_first_empty_line("cpp_program.cpp",struct)                                         # Since chatgpt removes pre defined structs for some reason, we store them ourselves and write to cpp file
+    append_to_first_empty_line("cpp_program.cpp",response)
+    compile_and_run_cpp_program()                                                               
   
 
 main()
